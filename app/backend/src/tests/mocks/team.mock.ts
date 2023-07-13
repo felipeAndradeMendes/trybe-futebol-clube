c1be4f89ea35c6a3261f842456b7f8036dd7e2f6
Felipe Mendes <felipemadruga@gmail.com>
2023-07-13 18:11:05 -0300

    Implementa teste de integração da rota get /teams

---
+++ app/backend/src/tests/mocks/team.mock.ts
@@ -0,0 +1,24 @@
+const teams = [
+  {
+    id: 1,
+    teamName: "Avaí/Kindermann"
+  },
+  {
+    id: 2,
+    teamName: "Bahia"
+  },
+  {
+    id: 3,
+    teamName: "Botafogo"
+  }  
+]
+
+const team = {
+  id: 1,
+  teamName: "Avaí/Kindermann"
+}
+
+export {
+  team,
+  teams,
+};
--- app/backend/src/tests/team.test.ts
+++ app/backend/src/tests/team.test.ts
@@ -1,45 +1,25 @@
-// import * as sinon from 'sinon';
-// import * as chai from 'chai';
-// // @ts-ignore
-// import chaiHttp = require('chai-http');
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
 
-// import { app } from '../app';
-// import Example from '../database/models/ExampleModel';
+import { app } from '../app';
+import SequelizeTeam from '../database/models/SequelizeTeam';
+import { teams } from './mocks/team.mock';
 
-// import { Response } from 'superagent';
+chai.use(chaiHttp);
 
-// chai.use(chaiHttp);
+const { expect } = chai;
 
-// const { expect } = chai;
+describe('TESTES ROTAS TEAM', () => {
+  function afterEach() {
+    sinon.restore();
+  }
+  it('A rota get /teams retorna um array de times', async function () {
+    sinon.stub(SequelizeTeam, 'findAll').resolves(teams as any);
+    const { status, body } = await chai.request(app).get('/teams');
 
-// describe('Seu teste', () => {
-//   /**
-//    * Exemplo do uso de stubs com tipos
-//    */
-
-//   // let chaiHttpResponse: Response;
-
-//   // before(async () => {
-//   //   sinon
-//   //     .stub(Example, "findOne")
-//   //     .resolves({
-//   //       ...<Seu mock>
-//   //     } as Example);
-//   // });
-
-//   // after(()=>{
-//   //   (Example.findOne as sinon.SinonStub).restore();
-//   // })
-
-//   // it('...', async () => {
-//   //   chaiHttpResponse = await chai
-//   //      .request(app)
-//   //      ...
-
-//   //   expect(...)
-//   // });
-
-//   it('Seu sub-teste', () => {
-//     expect(false).to.be.eq(true);
-//   });
-// });
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(teams);
+  });
+});
