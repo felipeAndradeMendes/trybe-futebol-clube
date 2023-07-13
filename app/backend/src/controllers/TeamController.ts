5f8f38169a068054bfc9009b0381f22dcfdf7d52
Felipe Mendes <felipemadruga@gmail.com>
2023-07-13 17:56:48 -0300

    Cria rota findAll teams
    resgata times com sucesso, a princpio

--- app/backend/src/controllers/TeamController.ts
+++ app/backend/src/controllers/TeamController.ts
@@ -1,14 +1,14 @@
-// import TeamService from '../services/TeamServices';
-// // import mapStatusHTTP from '../utils/mapStatusHTTP';
-// // import {} from 'express';
+import { Request, Response } from 'express';
+import TeamService from '../services/TeamServices';
+// import mapStatusHTTP from '../utils/mapStatusHTTP';
 
-// export default class TeamController {
-//   constructor(
-//     private teamService = new TeamService(),
-//   ) { }
+export default class TeamController {
+  constructor(
+    private teamService = new TeamService(),
+  ) { }
 
-//   public async getAllTeams(req: Request, res: Response){
-//     const serviceResponse = await this.teamService.getAllTeams();
-//     // return res.status(200).json(serviceResponse.data);
-//   }
-// }
+  public async getAllTeams(req: Request, res: Response) {
+    const serviceResponse = await this.teamService.getAllTeams();
+    return res.status(200).json(serviceResponse.data);
+  }
+}
