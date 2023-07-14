757eeb09b0c4ce6533ef03f356176b1656d1f9cd
Felipe Mendes <felipemadruga@gmail.com>
2023-07-14 09:52:47 -0300

    Implementa migration da tablea users

--- app/backend/src/Interfaces/Team.ts
+++ app/backend/src/Interfaces/Team.ts
@@ -4,7 +4,6 @@ export interface ITeam {
 }
 
 export interface ITeamModel {
-  // create(data: Partial<ITeam>): Promise<ITeam>,
   findAll(): Promise<ITeam[]>,
   findById(id: number): Promise<ITeam | null>,
 }
