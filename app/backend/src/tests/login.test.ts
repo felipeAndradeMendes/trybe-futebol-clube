fc83db52834f6fc6559390ab83f9167b46825cf0
Felipe Mendes <felipemadruga@gmail.com>
2023-07-14 15:55:03 -0300

    Implementa teste de erro em /login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -6,7 +6,14 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import * as bcrypt from 'bcryptjs';
 import SequelizeUser from '../database/models/SequelizeUser';
-import { user, validLoginBody } from './mocks/login.mock';
+import {
+  user,
+  validLoginBody,
+  noEmailLoginBody,
+  noPasswordLoginBody,
+  invalidEmailFormatLoginBody,
+  invalidPasswordFormatLoginBody,
+} from './mocks/login.mock';
 
 chai.use(chaiHttp);
 
@@ -20,21 +27,68 @@ describe('TESTES ROTA LOGIN', () => {
   
   it('O login é feito com sucesso', async function () {
     const mockFindOneReturn = SequelizeUser.build(user);
+
     sinon.stub(SequelizeUser, 'findOne').resolves(mockFindOneReturn);
     sinon.stub(bcrypt, 'compare').returns(true as any);
+
     const { status, body } = await chai.request(app).post('/login').send(validLoginBody);
     
     expect(status).to.equal(200);
     expect(body).to.have.key('token');
-  });    
+  });
+
+  it('Retorna erro caso email nao seja preenchido', async function () {
+    const { status, body } = await chai.request(app).post('/login').send(noEmailLoginBody);
+
+    expect(status).to.equal(400);
+    expect(body).to.deep.equal({ message: 'All fields must be filled'});
+  });
+
+  it('Retorna erro caso password nao seja preenchido', async function () {
+    const { status, body } = await chai.request(app).post('/login').send(noPasswordLoginBody);
+
+    expect(status).to.equal(400);
+    expect(body).to.deep.equal({ message: 'All fields must be filled'});
+  });
+
+  it('Retorna erro caso o formato do email esteja incorreto', async function () {
+    const { status, body } = await chai.request(app).post('/login').send(invalidEmailFormatLoginBody);
+
+    expect(status).to.equal(401);
+    expect(body).to.deep.equal({ message: 'Invalid email or password'});
+  });
+
+  it('Retorna erro caso o password tenha tenha menos de 7 caracteres', async function () {
+    const { status, body } = await chai.request(app).post('/login').send(invalidPasswordFormatLoginBody);
+
+    expect(status).to.equal(401);
+    expect(body).to.deep.equal({ message: 'Invalid email or password'});
+  });
+
+  it('Retorna erro quando o email não está cadastrado no banco de dados', async function () {
+    // const mockFindOneReturn = SequelizeUser.build(user);
+
+    sinon.stub(SequelizeUser, 'findOne').resolves(null);
+    sinon.stub(bcrypt, 'compare').returns(true as any);
+
+    const { status, body } = await chai.request(app).post('/login').send(validLoginBody);
+    
+    expect(status).to.equal(401);
+    expect(body).to.deep.equal({ message: 'Invalid email or password' });
+  });
+
+  it('Retorna erro quando a senha digitada está incorreta de acordo com o bd', async function () {
+    const mockFindOneReturn = SequelizeUser.build(user);
+
+    sinon.stub(SequelizeUser, 'findOne').resolves(mockFindOneReturn);
+    sinon.stub(bcrypt, 'compare').returns(false as any);
+
+    const { status, body } = await chai.request(app).post('/login').send(validLoginBody);
+    
+    expect(status).to.equal(401);
+    expect(body).to.deep.equal({ message: 'Invalid email or password' });
+  });
+  
 });
 
-// describe('ERROS ', function () {
-//   it('Deve retornar erro se o id do time buscado não existir no db', async function () {
-//     sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
-//     const { status, body } = await chai.request(app).get('/teams/999');
 
-//     expect(status).to.equal(404);
-//     expect(body).to.deep.equal({ message: `Não foi encontrado o time com id 999` })
-//   });
-// });
--- app/backend/src/tests/mocks/login.mock.ts
+++ app/backend/src/tests/mocks/login.mock.ts
@@ -6,39 +6,34 @@ const user = {
   password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'  
 }
 
-// const user1 = users {
-//   dataValues: {
-//     id: 2,
-//     username: 'User',
-//     role: 'user',
-//     email: 'user@user.com',
-//     password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
-//   },
-//   _previousDataValues: {
-//     id: 2,
-//     username: 'User',
-//     role: 'user',
-//     email: 'user@user.com',
-//     password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
-//   },
-//   uniqno: 1,
-//   _changed: Set(0) {},
-//   _options: {
-//     isNewRecord: false,
-//     _schema: null,
-//     _schemaDelimiter: '',
-//     raw: true,
-//     attributes: [ 'id', 'username', 'role', 'email', 'password' ]
-//   },
-//   isNewRecord: false
-// }
-
 const validLoginBody = {
   email: 'user@user.com',
   password: 'secret_user',
 }
 
+const noEmailLoginBody = {
+  password: 'secret_user',
+}
+
+const noPasswordLoginBody = {
+  email: 'user@user.com',
+}
+
+const invalidEmailFormatLoginBody = {
+  email: 'invalidemail@user',
+  password: 'secret_user',
+}
+
+const invalidPasswordFormatLoginBody = {
+  email: 'user@user.com',
+  password: '123456',
+}
+
 export {
   user,
   validLoginBody,
+  noEmailLoginBody,
+  noPasswordLoginBody,
+  invalidEmailFormatLoginBody,
+  invalidPasswordFormatLoginBody,
 };
