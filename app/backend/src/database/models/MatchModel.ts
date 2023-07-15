c623cf2dc1580ffc744b737a185dbc74cf4c0a14
Felipe Mendes <felipemadruga@gmail.com>
2023-07-15 19:31:14 -0300

    Corrige model match para apresentar chaves especificas

--- app/backend/src/database/models/MatchModel.ts
+++ app/backend/src/database/models/MatchModel.ts
@@ -1,11 +1,27 @@
 import SequelizeMatch from './SequelizeMatch';
 import { IMatch, IMatchModel } from '../../Interfaces/Match';
+import SequelizeTeam from './SequelizeTeam';
 
 export default class MatchModel implements IMatchModel {
   private model = SequelizeMatch;
 
   async findAll(): Promise<IMatch[]> {
-    const dbData = await this.model.findAll();
+    const dbData = await this.model.findAll(
+      {
+        include: [
+          {
+            model: SequelizeTeam,
+            as: 'homeTeam',
+            attributes: { exclude: ['id'] },
+          },
+          {
+            model: SequelizeTeam,
+            as: 'awayTeam',
+            attributes: { exclude: ['id'] },
+          },
+        ],
+      },
+    );
     return dbData;
   }
 }
