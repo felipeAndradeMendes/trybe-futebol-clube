ba09292265ec65160533e0cba5ac17fc02b18288
Felipe Mendes <felipemadruga@gmail.com>
2023-07-15 19:46:15 -0300

    Implementa controller getAllInProgress

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import MatchService from '../services/MatchService';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
 
 export default class MatchController {
   constructor(
@@ -8,6 +9,16 @@ export default class MatchController {
 
   public async getAllMatches(req: Request, res: Response): Promise<Response> {
     const serviceResponse = await this.matchService.getAllMatches();
+    return res.status(200).json(serviceResponse.data);
+  }
+
+  public async getAllInProgress(req: Request, res: Response): Promise<Response> {
+    const serviceResponse = await this.matchService.getAllInProgress();
+
+    if (serviceResponse.status !== 'SUCCESSFUL') {
+      return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+    }
+
     return res.status(200).json(serviceResponse.data);
   }
 }
